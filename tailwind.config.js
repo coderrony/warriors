1dc087cf6922d438e3c1b73e6c4f302a9fa7379d
rony7090 <rony7090@gmail.com>
2023-03-02 20:28:00 +0600

    new setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './templates/**/*.html',
+        'home/templates/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
